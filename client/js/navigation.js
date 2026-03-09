4b5246126de666102a7109ff2a6638f20e8751aa
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-09 11:35:09 -0600

    Update navigation.js

--- client/js/navigation.js
+++ client/js/navigation.js
@@ -1,23 +1,8 @@
-// Manejo de navegación global en PinolApp
-function irARestaurante(id) {
-    localStorage.setItem('restaurante_seleccionado', id);
+// client/js/navigation.js
+
+// Función para ir al restaurante seleccionado
+function verDetalleRestaurante(id) {
+    // Guardamos el ID en LocalStorage para persistencia
+    localStorage.setItem('restaurante_actual_id', id);
     window.location.href = 'restaurant.html';
 }
-
-function irAlCarrito() {
-    window.location.href = 'cart.html';
-}
-
-function verSeguimiento(pedidoId) {
-    localStorage.setItem('last_order_id', pedidoId);
-    window.location.href = 'tracking.html';
-}
-
-// Para que el botón de "Comienza a pedir" del index funcione
-const btnComenzar = document.querySelector('.btn-comenzar');
-if(btnComenzar) {
-    btnComenzar.addEventListener('click', () => {
-        window.location.href = 'login.html';
-    });
-}
-
