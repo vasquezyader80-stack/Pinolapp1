96338cfcd6265b6bbe9a308840514e05b6d6aede
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-09 14:03:59 -0600

    Update supabase-config.js

--- client/js/supabase-config.js
+++ client/js/supabase-config.js
@@ -1,5 +1,4 @@
-// client/js/supabase-config.js
-const supabaseUrl = 'https://xcfebmqpcshkrfascaqb.supabase.co'; // Tu URL
-const supabaseKey = 'sb_publishable_C7hmzFXYzmkKBN0N5vFF2Q_a3ONnTxf'; // Tu llave pegada aquí
+const supabaseUrl = 'https://xcfebmqpcshkrfascaqb.supabase.co';
+const supabaseKey = 'sb_publishable_C7hmzFXYzmkKBN0N5vFF2Q_a3ONnTxf';
 
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
