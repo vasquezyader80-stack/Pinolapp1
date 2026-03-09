2449f833f5dd8c96f6e0b6861cbed3c4bff40588
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-09 14:36:18 -0600

    Update supabase-config.js

--- client/js/supabase-config.js
+++ client/js/supabase-config.js
@@ -2,3 +2,6 @@ const supabaseUrl = 'https://xcfebmqpcshkrfascaqb.supabase.co';
 const supabaseKey = 'sb_publishable_C7hmzFXYzmkKBN0N5vFF2Q_a3ONnTxf';
 
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Esto nos avisará en la consola del navegador si la conexión es exitosa
+console.log("PinolApp conectada exitosamente a la base de datos.");
